42645aab2c71e15c19755b70d62dceb9fae93e7a
Faris <f.abualnaja15@gmail.com>
2023-02-26 12:57:49 +0000

    Modified admin.py to remove the last post.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 from .models import Post
-from django.contrib.auth.models import User
-
-me = User.objects.get(username='faris')
-post1 = Post.objects.create(author=me, title='My first blog post', text='This is a blog. I am writing a blog. This blog is short. Thank you for reading my blog.')
-post1.publish()
 
 admin.site.register(Post)
